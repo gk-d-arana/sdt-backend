8c6a1205d1daeac48a00de3758a88eac52522464
gk-arana <cissci12@gmail.com>
2021-10-18 20:36:31 +0300

    almost done

--- sdtSections/urls.py
+++ sdtSections/urls.py
@@ -3,7 +3,7 @@ from .views import *
 urlpatterns = [
     path('main_sections', get_all_main_sections_view),
     path('main_section/<int:pk>/sections', get_all_sections_view),
-    path('section/<int:pk>/products', get_all_products_view),
+    path('section/<int:pk>/products/', get_all_products_view),
     path('sections/', SectionListView.as_view()),
     path('latest_products/', LatestProductsListView.as_view()),
 
